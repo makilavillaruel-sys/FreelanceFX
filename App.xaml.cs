81e4bdeae5ea5474a6c6ffc59e3d4444237b2e5e
makilavillaruel-sys <makilavillaruel@gmail.com>
2026-04-05 22:19:58 -0400

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,1 +1,10 @@
-
+namespace FreeLanceFX
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
