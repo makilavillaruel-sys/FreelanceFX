f161a521c77ebd3eea889a29400b9bc9ce8928c0
makilavillaruel-sys <makilavillaruel@gmail.com>
2026-04-05 22:18:50 -0400

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,1 +1,15 @@
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 
+namespace CurrencyApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>();
+            return builder.Build();
+        }
+    }
+}
